7fdac99213a37bf1ad6b3b41da585a5c37f250bf
Kettengeschwindigkeit <666damned999@mail.ru>
2023-12-17 15:42:07 +0300

    full project

---
+++ front-end/src/home/Home.tsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import styles from "./Home.module.css";
+
+interface IFormState {
+  name: string;
+  email: string;
+}
+
+function Home() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [isSuccess, setIsSuccess] = useState(false);
+
+  const { register, reset, handleSubmit } = useForm<IFormState>();
+
+  const onSubmit: SubmitHandler<IFormState> = data => {
+    setIsLoading(true);
+
+    fetch("http://localhost:5000/api", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data)
+    }).then(response => response.json())
+      .then(data => {
+        if (!data) return;
+
+        setIsSuccess(true);
+        reset();
+      })
+      .finally(() => setIsLoading(false));
+  };
+
+  return (
+    <div className={styles.wrapper}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {isSuccess ? (
+          <div className={styles.success}>Форма отправлена!</div>
+        ) : (
+          <>
+            <h1>GTA 6 - Оставь заявку</h1>
+            <input type="name" placeholder="Введите имя:" {...register("name")} />
+            <input type="email" placeholder="Введите Email:" {...register("email")} />
+            <button disabled={isLoading}>{isLoading ? "Отправка" : "Хочу ГТА!"}</button>
+          </>
+        )}
+      </form>
+    </div>
+  );
+}
+
+export default Home;
